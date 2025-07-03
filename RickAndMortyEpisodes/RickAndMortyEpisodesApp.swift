179aca1696489a635a6fa6c9ad50c467343523cd
Alessio Roberto <darthpelo@gmail.com>
2025-07-03 11:35:16 +0200

    Initial commit

---
+++ RickAndMortyEpisodes/RickAndMortyEpisodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyEpisodesApp.swift
+//  RickAndMortyEpisodes
+//
+//  Created by Alessio Roberto on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyEpisodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyEpisodesTests/RickAndMortyEpisodesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyEpisodesTests.swift
+//  RickAndMortyEpisodesTests
+//
+//  Created by Alessio Roberto on 03/07/2025.
+//
+
+import Testing
+@testable import RickAndMortyEpisodes
+
+struct RickAndMortyEpisodesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
